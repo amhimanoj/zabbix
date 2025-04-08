6962b4003e72c57f8ac4f024aa41e301a9ee43a1
amhimanoj <85225127+amhimanoj@users.noreply.github.com>
2025-04-08 21:49:39 +0530

    Update debian-install70.sh

--- debian-install70.sh
+++ debian-install70.sh
@@ -6,7 +6,7 @@ wget https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/zabbix
 sudo apt install ./zabbix-release_latest_7.0+debian12_all.deb -y && sudo apt update
 sudo apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-nginx-conf zabbix-sql-scripts zabbix-agent2
 sudo mysql -uroot -p'changeme' -e "CREATE DATABASE zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin; CREATE USER 'zabbix'@'localhost' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost'; SET GLOBAL log_bin_trust_function_creators = 1;"
-zcat /usr/share/zabbix/sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p'password' zabbix
+zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p'password' zabbix
 sudo mysql -uroot -p'changeme' -e "set global log_bin_trust_function_creators = 0;"
 echo "DBPassword=password" | sudo tee -a /etc/zabbix/zabbix_server.conf > /dev/null
 sudo sed -i -e 's/^#\s*\(listen\s\+\)/\1/' -e 's/^#\s*\(server_name\s\+\)/\1/' /etc/zabbix/nginx.conf
