1b7bb547b8aeaf3db63e6f749704cff01b52234b
amhimanoj <85225127+amhimanoj@users.noreply.github.com>
2025-04-08 21:35:53 +0530

    Update debian-install70.sh

--- debian-install70.sh
+++ debian-install70.sh
@@ -1,4 +1,3 @@
-sudo apt update
 sudo apt install -y locales
 echo "en_US.UTF-8 UTF-8" | sudo tee /etc/locale.gen
 sudo locale-gen
@@ -6,9 +5,6 @@ sudo update-locale LANG=en_US.UTF-8
 wget https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.0+debian12_all.deb
 sudo apt install ./zabbix-release_latest_7.0+debian12_all.deb -y && sudo apt update
 sudo apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-nginx-conf zabbix-sql-scripts zabbix-agent2
-sudo apt install -y mariadb-server
-sudo systemctl enable mariadb 
-sudo systemctl start mariadb
 sudo mysql -uroot -p'changeme' -e "CREATE DATABASE zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin; CREATE USER 'zabbix'@'localhost' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost'; SET GLOBAL log_bin_trust_function_creators = 1;"
 zcat /usr/share/zabbix/sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p'password' zabbix
 sudo mysql -uroot -p'changeme' -e "set global log_bin_trust_function_creators = 0;"
